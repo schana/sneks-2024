9d8cbbead786be9be805afdefc5478012ccf813b
Nathaniel Schaaf <schana@users.noreply.github.com>
2024-03-03 00:27:03 -0600

    Clean up build for distribution

--- src/submission/submission/snek/submission.py
+++
@@ -1,7 +0,0 @@
-from sneks.engine.core.direction import Direction
-from sneks.engine.interface.snek import Snek
-
-
-class CustomSnek(Snek):
-    def get_next_direction(self) -> Direction:
-        return Direction.UP
--- src/webapp/make.py
+++ src/webapp/make.py
@@ -26,24 +26,27 @@ def build_docs():
             "-o",
             "modules",
             sneks_path,
-            f"{sneks_path}/submission",
-            f"{sneks_path}/infrastructure",
+            # exclude the following
             f"{sneks_path}/backend",
+            f"{sneks_path}/engine/config",
             f"{sneks_path}/engine/engine",
             f"{sneks_path}/engine/gui",
             f"{sneks_path}/engine/validator",
-            f"{sneks_path}/engine/config",
-            f"{sneks_path}/engine/template",
+            f"{sneks_path}/infrastructure",
         ]
     )
     subprocess.run(["sphinx-build", "-b", "html", ".", "build/docs"])
 
 
 def get_template_readme():
-    root_path = pathlib.Path(".").resolve().parent.parent
     prefix = pathlib.Path("modules")
     prefix.mkdir(exist_ok=True)
-    shutil.copyfile(root_path / "README.md", prefix / "README.md")
+    response = requests.get(
+        "https://github.com/schana/toroidal-sneks-submission/raw/main/README.md"
+    )
+    with open(prefix / "README.md", "wb") as f:
+        for chunk in response.iter_content():
+            f.write(chunk)
 
 
 def get_template():
@@ -51,7 +54,7 @@ def get_template():
     shutil.rmtree(prefix, ignore_errors=True)
     prefix.mkdir(exist_ok=True)
     response = requests.get(
-        "https://github.com/schana/sneks-submission/archive/refs/heads/main.zip"
+        "https://github.com/schana/toroidal-sneks-submission/archive/refs/heads/main.zip"
     )
     with open(prefix / "template.zip", "wb") as f:
         for chunk in response.iter_content():
